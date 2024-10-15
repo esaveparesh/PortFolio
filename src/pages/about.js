c7b3deb0ba7a4750f014d7f6c8c22cd0caf2df43
Paresh Esave <esaveparesh@gmail.com>
2024-10-15 20:27:40 +0530

    commit

---
+++ src/pages/about.js
@@ -0,0 +1,107 @@
+import AnimatedText from "@/components/AnimatedText";
+import Layout from "@/components/Layout";
+import Head from "next/head";
+import React, { useEffect, useRef } from "react";
+import profilePic from "../../public/images/profile/developer-pic-4.png";
+import Image from "next/image";
+import { useInView, useMotionValue, useSpring } from "framer-motion";
+import Skills from "@/components/Skills";
+import Experience from "@/components/Experience";
+import Education from "@/components/Education";
+
+const AnimatedNumbers = ({ value }) => {
+  const ref = useRef(null);
+
+  const motionValue = useMotionValue(0);
+  const springValue = useSpring(motionValue, { duration: 3000 });
+  const isInView = useInView(ref, { once: true });
+
+  useEffect(() => {
+    if (isInView) {
+      motionValue.set(value);
+    }
+  }, [isInView, value, motionValue]);
+
+  useEffect(() => {
+    springValue.on("change", (latest) => {
+      if (ref.current && latest.toFixed(0) <= value) {
+        ref.current.textContent = latest.toFixed(0);
+      }
+    });
+  }, [springValue, value]);
+
+  return <span ref={ref}></span>;
+};
+
+const about = () => {
+  return (
+    <>
+      <Head>
+        <title>Paresh Esave | About Page</title>
+        <meta name="description" content="any description" />
+      </Head>
+      <main className="flex w-full flex-col items-center justify-center">
+        <Layout className="pt-16">
+          <AnimatedText text="Passion Fuels Purpose!" className="mb-16" />
+          <div className="grid w-full grid-cols-8 gap-16">
+            <div className="col-span-3 flex flex-col items-start justify-start">
+              <h2 className="mb-4 text-lg font-bold uppercase text-dark/75">
+                Biography
+              </h2>
+
+              <p className="font-medium">
+                Being a passionate MERN Stack Developer, I specialize in
+                dynamic, high-performance web applications using MongoDB,
+                Express.js, React.js and Node.js.
+              </p>
+              <p className="my-4 font-medium">
+                As someone with a strong foundation in full-stack development I
+                enjoy solving complex problems and delivering robust solutions
+                that are scalable too.
+              </p>
+              <p className="font-medium">
+                My portfolio ranges from single-page apps to intricate
+                multi-user platforms. I am always thrilled to tackle new
+                challenges as well as keep myself updated with the newest
+                technologies around web development
+              </p>
+            </div>
+
+            <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark p-8">
+              <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark" />
+              <Image
+                src={profilePic}
+                alt="Codebucks"
+                className="w-full h-auto rounded-2xl"
+              />
+            </div>
+
+            <div className="col-span-2 flex flex-col items-end justify-between">
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold">
+                  <AnimatedNumbers value={2} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  Interships Completed
+                </h2>
+              </div>
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold">
+                  <AnimatedNumbers value={10} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  projects completed
+                </h2>
+              </div>
+            </div>
+          </div>
+          <Skills />
+          <Experience />
+          <Education />
+        </Layout>
+      </main>
+    </>
+  );
+};
+
+export default about;
