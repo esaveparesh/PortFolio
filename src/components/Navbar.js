afbc33f431037a347db9fd3f680e65e79ada77c0
Paresh Esave <esaveparesh@gmail.com>
2024-10-18 21:46:43 +0530

    Second commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import Logo from "./Logo";
 import { useRouter } from "next/router";
 import {
@@ -8,8 +8,11 @@ import {
   LinkedInIcon,
   PinterestIcon,
   DribbbleIcon,
+  SunIcon,
+  MoonIcon,
 } from "./Icons";
 import { motion } from "framer-motion";
+import useThemeSwitcher from "./hooks/useThemeSwitcher";
 
 const CustomLink = ({ href, title, className = "" }) => {
   const router = useRouter();
@@ -22,7 +25,7 @@ const CustomLink = ({ href, title, className = "" }) => {
         transition-[width] ease duration-300
         
 				${router.asPath === href ? "w-full" : "w-0"}
-				`}
+        dark:bg-light`}
       >
         &nbsp;
       </span>
@@ -31,8 +34,23 @@ const CustomLink = ({ href, title, className = "" }) => {
 };
 
 const Navbar = () => {
+  const [mode, setMode] = useThemeSwitcher();
+  const [isOpen, setisOpen] = useState(false)
+
+  const handleClick = ()=>{
+    setisOpen(!isOpen)
+  }
+
   return (
-    <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
+    <header className="w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light relative">
+
+      <button className="flex-col justify-center items-center hidden lg:flex" onClick={handleClick}>
+        <span className={`bg-dark dark:bg-light  transition-all duration-300 ease-out h-0.5 w-6 rounded-sm -translate-y-0.5 ${isOpen ? 'rotate-45 translate-y-1':'-translate-y-0.5'}`}></span>
+        <span className={`bg-dark dark:bg-light  transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${isOpen ? 'opacity-0' : 'opacity-100'}`}></span>
+        <span className={`bg-dark dark:bg-light  transition-all duration-300 ease-out h-0.5 w-6 rounded-sm translate-y-0.5 ${isOpen ? '-rotate-45 -translate-y-1':'translate-y-0.5'}`}></span>
+      </button>
+    
+      <div className="min-w-[70vm] flex flex-col justify-between items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       <nav>
         <CustomLink href="/" title="Home" className="mr-4" />
         <CustomLink href="/about" title="About" className="mx-4" />
@@ -72,7 +90,7 @@ const Navbar = () => {
           href="https://pinterest.com"
           target={"_blank"}
           whileHover={{ y: -2 }}
-          className="w-6 mx-3"
+          className="w-6 mx-3 bg-light rounded-full"
           whileTap={{ scale: 0.9 }}
         >
           <PinterestIcon />
@@ -86,7 +104,88 @@ const Navbar = () => {
         >
           <DribbbleIcon />
         </motion.a>
+
+        <button 
+        onClick={() => setMode(mode === "light" ? "dark" : "light")}
+        className={`ml-3 flex items-center justify-center rounded-full p-1 ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}`} 
+        >
+          {mode === "dark" ? (
+            <SunIcon className={"fill-dark"} />
+          ) : (
+            <MoonIcon className={"fill-dark"} />
+          )}
+        </button>
       </nav>
+      </div>
+
+      <div className="w-full flex justify-between items-center lg:hidden">
+      <nav>
+        <CustomLink href="/" title="Home" className="mr-4" />
+        <CustomLink href="/about" title="About" className="mx-4" />
+        <CustomLink href="/projects" title="Project" className="mx-4" />
+        <CustomLink href="/articles" title="Articles" className="ml-4" />
+      </nav>
+
+      <nav className="flex items-center justify-center flex-wrap">
+        <motion.a
+          href="https://twitter.com"
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 mr-3"
+        >
+          <TwitterIcon />
+        </motion.a>
+        <motion.a
+          href="https://github.com"
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          className="w-6 mx-3"
+          whileTap={{ scale: 0.9 }}
+        >
+          <GithubIcon />
+        </motion.a>
+        <motion.a
+          href="https://linkedin.com"
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          className="w-6 mx-3"
+          whileTap={{ scale: 0.9 }}
+        >
+          <LinkedInIcon />
+        </motion.a>
+        <motion.a
+          href="https://pinterest.com"
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          className="w-6 mx-3 bg-light rounded-full"
+          whileTap={{ scale: 0.9 }}
+        >
+          <PinterestIcon />
+        </motion.a>
+        <motion.a
+          href="https://dribbble.com"
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          className="w-6 ml-3"
+          whileTap={{ scale: 0.9 }}
+        >
+          <DribbbleIcon />
+        </motion.a>
+
+        <button 
+        onClick={() => setMode(mode === "light" ? "dark" : "light")}
+        className={`ml-3 flex items-center justify-center rounded-full p-1 ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}`} 
+        >
+          {mode === "dark" ? (
+            <SunIcon className={"fill-dark"} />
+          ) : (
+            <MoonIcon className={"fill-dark"} />
+          )}
+        </button>
+      </nav>
+      </div>
+
       <div className="absolute left-[50%] top-2 translate-x-[-50%]">
         <Logo />
       </div>
--- src/pages/about.js
+++ src/pages/about.js
@@ -40,12 +40,12 @@ const about = () => {
         <title>Paresh Esave | About Page</title>
         <meta name="description" content="any description" />
       </Head>
-      <main className="flex w-full flex-col items-center justify-center">
+      <main className="flex w-full flex-col items-center justify-center dark:text-light">
         <Layout className="pt-16">
           <AnimatedText text="Passion Fuels Purpose!" className="mb-16" />
           <div className="grid w-full grid-cols-8 gap-16">
             <div className="col-span-3 flex flex-col items-start justify-start">
-              <h2 className="mb-4 text-lg font-bold uppercase text-dark/75">
+              <h2 className="mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75">
                 Biography
               </h2>
 
@@ -67,12 +67,16 @@ const about = () => {
               </p>
             </div>
 
-            <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark p-8">
-              <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark" />
+            <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark p-8  dark:bg-dark dark:border-light">
+              <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light" />
               <Image
                 src={profilePic}
-                alt="Codebucks"
+                alt="profile Pic"
                 className="w-full h-auto rounded-2xl"
+                priority
+                sizes="(max-width:768px),100vm,
+                (max-width:1200px),50vm,
+                33vm"
               />
             </div>
 
@@ -81,7 +85,7 @@ const about = () => {
                 <span className="inline-block text-7xl font-bold">
                   <AnimatedNumbers value={2} />+
                 </span>
-                <h2 className="text-xl font-medium capitalize text-dark/75">
+                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light">
                   Interships Completed
                 </h2>
               </div>
@@ -89,7 +93,7 @@ const about = () => {
                 <span className="inline-block text-7xl font-bold">
                   <AnimatedNumbers value={10} />+
                 </span>
-                <h2 className="text-xl font-medium capitalize text-dark/75">
+                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light">
                   projects completed
                 </h2>
               </div>
