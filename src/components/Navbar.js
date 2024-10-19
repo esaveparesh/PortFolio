a7f047fa82912d4fc8dc227b171a1348854fb97a
Paresh Esave <esaveparesh@gmail.com>
2024-10-19 13:23:15 +0530

    modified

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -76,7 +76,7 @@ const Navbar = () => {
         onClick={handleClick}
       >
         <span
-          className={`bg-dark dark:bg-light  transition-all duration-300 ease-out h-0.5 w-6 rounded-sm -translate-y-0.5 ${
+          className={`bg-dark dark:bg-light  transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${
             isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5"
           }`}
         ></span>
@@ -86,7 +86,7 @@ const Navbar = () => {
           }`}
         ></span>
         <span
-          className={`bg-dark dark:bg-light  transition-all duration-300 ease-out h-0.5 w-6 rounded-sm translate-y-0.5 ${
+          className={`bg-dark dark:bg-light  transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${
             isOpen ? "-rotate-45 -translate-y-1" : "translate-y-0.5"
           }`}
         ></span>
