9dbe08a669bc883e1153aff4a7cb8fed79155334
Paresh Esave <esaveparesh@gmail.com>
2024-10-19 13:12:27 +0530

    modified

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -13,6 +13,7 @@ import article5 from "../../public/images/articles/smooth scrolling in reactjs.p
 import article6 from "../../public/images/articles/todo list app built using react redux and framer motion.png";
 import article7 from "../../public/images/articles/What is Redux with easy explanation.png";
 import article8 from "../../public/images/articles/What is higher order component in React.jpg";
+import TransitionEffect from "@/components/TransitionEffect";
 
 const FramerImage = motion(Image);
 
@@ -50,7 +51,7 @@ const MovingImg = ({ title, img, link }) => {
         ref={imgRef}
         src={img}
         alt={title}
-        className="z-10 w-96 h-auto hidden absolute rounded-lg"
+        className="z-10 w-96 h-auto hidden absolute rounded-lg md:!hidden"
       />
     </Link>
   );
@@ -63,10 +64,11 @@ const Article = ({ img, title, date, link }) => {
       whileInView={{ y: 0, transition: { duration: 0.5, ease: "easeInOut" } }}
       viewport={{ once: true }}
       className="relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 
-      border border-solid border-dark border-r-4 border-b-4 dark:border-light dark:bg-dark dark:text-light"
+      border border-solid border-dark border-r-4 border-b-4 dark:border-light dark:bg-dark dark:text-light
+      sm:flex-col "
     >
       <MovingImg title={title} img={img} link={link} />
-      <span className="text-primary font-semibold pl-4 dark:text-primaryDark">{date}</span>
+      <span className="text-primary font-semibold pl-4 dark:text-primaryDark sm:self-start sm:pl-0 xs:text-sm">{date}</span>
     </motion.li>
   );
 };
@@ -89,7 +91,7 @@ const FeaturedArticle = ({ img, title, time, summary, link }) => {
         />
       </Link>
       <Link href={link} target="_blank">
-        <h2 className="capitalize text-2xl font-bold my-2 hover:underline">
+        <h2 className="capitalize text-2xl font-bold my-2 hover:underline xs:text-lg">
           {title}
         </h2>
       </Link>
@@ -106,10 +108,11 @@ const articles = () => {
         <title>Paresh Esave | Articles Page</title>
         <meta name="description" content="any description" />
       </Head>
+      <TransitionEffect/>
       <main className="w-full mb-16 flex flex-col items-center justify-center overflow-hidden dark:text-light">
         <Layout className="pt-16">
-          <AnimatedText text="Words Can Change the World!" className="mb-16" />
-          <ul className="grid grid-cols-2 gap-16">
+          <AnimatedText text="Words Can Change the World!" className="mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl" />
+          <ul className="grid grid-cols-2 gap-16 md:grid-cols-1 lg:gap-8 md:gap-y-16">
             <FeaturedArticle
               title="Build A Custom Pagination Component In Reactjs From Scratch"
               summary="Learn how to build a custom pagination component in ReactJS from scratch. 
